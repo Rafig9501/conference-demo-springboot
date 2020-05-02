63c6f1a8d15b0d05c75aa43957be0a0537f08af6
Rafig <rafiqmammedzade@gmail.com>
2020-05-02 22:37:37 +0400

    initial commit

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -1,13 +1,11 @@
 package com.pluralsight.conferencedemo;
 
-import com.pluralsight.conferencedemo.model.SessionModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConferenceDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ConferenceDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceDemoApplication.class, args);
+    }
 }
